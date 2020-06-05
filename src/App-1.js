4c5a560fdcd0aeb28a121d6f2d5eba44a23a0b53
stuartambient <sambient@gmail.com>
2020-06-05 14:10:12 -0600

    added a few lines-2

--- src/App-1.js
+++ src/App-1.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+// eslint-disable-next-line
+const title = "React";
+const welcome = {
+  greeting: "Hey",
+  title: "React and React-Native",
+};
+
+const useFunction = (param) => param;
+const element = <p>Hello again!</p>;
+const myarray = [<p>Hello</p>, <p>World</p>, <p>Again!!!!!!</p>, 4, 5, 6];
+
 const list = [
   {
     title: "React",
@@ -22,24 +33,16 @@ const list = [
 function App() {
   return (
     <div>
-      <h1>My Hacker Stories</h1>
+      <h1>Hello {title} World</h1>
+      <h2>
+        {welcome.greeting} {welcome.title}
+      </h2>
+      <h3>{useFunction("myFunction")}</h3>
+      <h4>{element}</h4>
+      <div>{myarray}</div>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-
-      <hr />
-      {list.map(function (item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
     </div>
   );
 }
